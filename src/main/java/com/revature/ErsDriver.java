6ff4290c3b4a1a0bae2b0f021f696d4623262441
seaboyz <seaboyz@hotmail.com>
2022-04-04 21:08:29 -0500

    Add simple command line interface for printing options and login and quit

--- src/main/java/com/revature/ErsDriver.java
+++ src/main/java/com/revature/ErsDriver.java
@@ -1,8 +1,47 @@
 package com.revature;
 
+import java.util.Scanner;
+
 public class ErsDriver {
 
-    public static void main(String[] args) {
-        System.out.println("testing");
+  static Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+
+    System.out.println("Welcome to ERS!");
+    boolean quit = false;
+
+    while (!quit) {
+      printOptions();
+      int choice = scanner.nextInt();
+      switch (choice){
+        case 1:
+          login();
+          break;
+        case 0:
+          quit = true;
+          break;
+        default:
+          printOptions();
+      }
     }
+  }
+
+  static void login(){
+    System.out.println("Please Enter your email:");
+    scanner.nextLine();
+    String email = scanner.nextLine();
+    System.out.println("Your email is: " + email);
+    System.out.println("Please Enter your password: ");
+    String password = scanner.nextLine();
+    System.out.println("Your password is: " + password);
+    System.out.println("Checking your password...");
+  }
+
+  static void printOptions(){
+    System.out.println("Press 1 to login");
+    System.out.println("press 2 to quit");
+  }
+
+
 }
