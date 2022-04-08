9509c650b28fedef4e9d1f9e55d3394a42e58aee
seaboyz <seaboyz@hotmail.com>
2022-04-07 20:24:38 -0500

    Add MockDB to getByUsername service

--- src/main/java/com/revature/services/MockDB.java
+++ src/main/java/com/revature/services/MockDB.java
@@ -1,5 +1,7 @@
 package com.revature.services;
 
+import com.revature.models.Role;
+import com.revature.models.User;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -8,15 +10,45 @@ import org.json.simple.parser.ParseException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MockDB {
-  public static void main(String[] args) {
+  private JSONParser jsonParser = new JSONParser();
+  private JSONArray users;
+  private static  MockDB db;
 
-    JSONParser jsonParser = new JSONParser();
+  public static MockDB getInstance() {
+    if(db==null){
+      db = new MockDB();
+    }
+    return db;
+  }
+
+  public ArrayList<User> findAll() {
+    loadUserData();
+    ArrayList<User> userArrayList = new ArrayList<>();
+
+    for (Object user : users) {
+      JSONObject userJsonObject = ((JSONObject) user);
+      int id = ((Long) userJsonObject.get("id")).intValue();
+      String username = userJsonObject.get("username").toString();
+      String password = userJsonObject.get("password").toString();
+      Role role = Role.valueOf((userJsonObject.get("role").toString().toUpperCase()));
+      userArrayList.add(new User(id, username, password, role));
+    }
+    return userArrayList;
+  }
+
+  public User getUserByUsername(String username) {
+    ArrayList<User> users = findAll();
+    return users.stream().filter(user -> user.getUsername().equals(username)).findAny().orElse(null);
+  }
+
+  private void loadUserData() {
     try (FileReader reader = new FileReader("src/main/resources/user.json")) {
       Object obj = jsonParser.parse(reader);
-      JSONArray userList = (JSONArray) obj;
-      userList.forEach(user -> parseUserObject((JSONObject) user));
+      users = (JSONArray) obj;
+
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     } catch (ParseException e) {
@@ -25,13 +57,14 @@ public class MockDB {
       e.printStackTrace();
     }
   }
-  private static void parseUserObject(JSONObject user) {
+
+  private void parseUserObject(JSONObject user) {
     String name = (String) user.get("name");
     String firstname = name.split(" ")[0];
     String lastname = name.split(" ")[1];
     String username = (String) user.get("username");
     String email = ((String) user.get("email"));
-    System.out.println(email);
+
 
   }
 
--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -1,15 +1,15 @@
 package com.revature.services;
 
-import java.util.Optional;
-
 import com.revature.models.User;
+
+import java.util.Optional;
 
 /**
  * The UserService should handle the processing and retrieval of Users for the ERS application.
- *
+ * <p>
  * {@code getByUsername} is the only method required;
  * however, additional methods can be added.
- *
+ * <p>
  * Examples:
  * <ul>
  *     <li>Create User</li>
@@ -20,11 +20,13 @@ import com.revature.models.User;
  * </ul>
  */
 public class UserService {
+  private MockDB mockDB = MockDB.getInstance();
 
-	/**
-	 *     Should retrieve a User with the corresponding username or an empty optional if there is no match.
-     */
-	public Optional<User> getByUsername(String username) {
-		return Optional.empty();
-	}
+  /**
+   * Should retrieve a User with the corresponding username or an empty optional if there is no match.
+   */
+  public Optional<User> getByUsername(String username) {
+    User user = mockDB.getUserByUsername(username);
+    return user!=null ? Optional.of(user) : Optional.empty();
+  }
 }
