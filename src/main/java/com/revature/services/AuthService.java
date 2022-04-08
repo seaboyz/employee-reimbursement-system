47adeed12487199dbcebe38c3a0ca69d4e1c67ad
seaboyz <seaboyz@hotmail.com>
2022-04-08 13:31:40 -0500

    Add dependency injection of UserRepository to AuthService

--- src/main/java/com/revature/services/AuthService.java
+++ src/main/java/com/revature/services/AuthService.java
@@ -3,14 +3,15 @@ package com.revature.services;
 import com.revature.exceptions.UserNamePasswordNotMatchException;
 import com.revature.exceptions.UserNotExistException;
 import com.revature.models.User;
+import com.revature.repositories.UserRepository;
 
 import java.util.Optional;
 
 /**
  * The AuthService should handle login and registration for the ERS application.
- *
+ * <p>
  * {@code login} and {@code register} are the minimum methods required; however, additional methods can be added.
- *
+ * <p>
  * Examples:
  * <ul>
  *     <li>Retrieve Currently Logged-in User</li>
@@ -20,51 +21,59 @@ import java.util.Optional;
  */
 public class AuthService {
 
-    /**
-     * <ul>
-     *     <li>Needs to check for existing users with username/email provided.</li>
-     *     <li>Must throw exception if user does not exist.</li>
-     *     <li>Must compare password provided and stored password for that user.</li>
-     *     <li>Should throw exception if the passwords do not match.</li>
-     *     <li>Must return user object if the user logs in successfully.</li>
-     * </ul>
-     */
-    public User login(String username, String password) {
-        UserService userService = new UserService();
-        Optional<User> userOptional = userService.getByUsername(username);
-        if(userOptional.isPresent()){
-            User user = userOptional.get();
-            if(user.getUsername().equals(username)&& user.getPassword().equals(password)){
-                return user;
-            }
-            throw new UserNamePasswordNotMatchException();
-        }
-        throw new UserNotExistException();
-    }
+  /**
+   * <ul>
+   *     <li>Needs to check for existing users with username/email provided.</li>
+   *     <li>Must throw exception if user does not exist.</li>
+   *     <li>Must compare password provided and stored password for that user.</li>
+   *     <li>Should throw exception if the passwords do not match.</li>
+   *     <li>Must return user object if the user logs in successfully.</li>
+   * </ul>
+   */
 
-    /**
-     * <ul>
-     *     <li>Should ensure that the username/email provided is unique.</li>
-     *     <li>Must throw exception if the username/email is not unique.</li>
-     *     <li>Should persist the user object upon successful registration.</li>
-     *     <li>Must throw exception if registration is unsuccessful.</li>
-     *     <li>Must return user object if the user registers successfully.</li>
-     *     <li>Must throw exception if provided user has a non-zero ID</li>
-     * </ul>
-     *
-     * Note: userToBeRegistered will have an id=0, additional fields may be null.
-     * After registration, the id will be a positive integer.
-     */
-    public User register(User userToBeRegistered) {
-        return null;
-    }
+  private UserService userService;
+  private UserRepository userRepository;
 
-    /**
-     * This is an example method signature for retrieving the currently logged-in user.
-     * It leverages the Optional type which is a useful interface to handle the
-     * possibility of a user being unavailable.
-     */
-    public Optional<User> exampleRetrieveCurrentUser() {
-        return Optional.empty();
+  public AuthService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+    this.userService = new UserService(userRepository);
+  }
+
+  public User login(String username, String password) {
+    Optional<User> userOptional = userService.getByUsername(username);
+    if (userOptional.isPresent()) {
+      User user = userOptional.get();
+      if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+        return user;
+      }
+      throw new UserNamePasswordNotMatchException();
     }
+    throw new UserNotExistException();
+  }
+
+  /**
+   * <ul>
+   *     <li>Should ensure that the username/email provided is unique.</li>
+   *     <li>Must throw exception if the username/email is not unique.</li>
+   *     <li>Should persist the user object upon successful registration.</li>
+   *     <li>Must throw exception if registration is unsuccessful.</li>
+   *     <li>Must return user object if the user registers successfully.</li>
+   *     <li>Must throw exception if provided user has a non-zero ID</li>
+   * </ul>
+   * <p>
+   * Note: userToBeRegistered will have an id=0, additional fields may be null.
+   * After registration, the id will be a positive integer.
+   */
+  public User register(User userToBeRegistered) {
+    return null;
+  }
+
+  /**
+   * This is an example method signature for retrieving the currently logged-in user.
+   * It leverages the Optional type which is a useful interface to handle the
+   * possibility of a user being unavailable.
+   */
+  public Optional<User> exampleRetrieveCurrentUser() {
+    return Optional.empty();
+  }
 }
