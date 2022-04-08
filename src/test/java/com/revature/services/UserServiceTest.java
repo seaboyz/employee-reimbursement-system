aaec0f4b1522ca9c52ebd06f667136b0303d84ab
seaboyz <seaboyz@hotmail.com>
2022-04-08 10:58:16 -0500

    Add UserServiceTest

--- src/test/java/com/revature/services/UserServiceTest.java
+++ src/test/java/com/revature/services/UserServiceTest.java
@@ -1,19 +1,16 @@
 package com.revature.services;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.*;
-
-import java.util.Optional;
-
+import com.revature.models.Role;
+import com.revature.models.User;
 import com.revature.repositories.UserRepository;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.revature.models.Role;
-import com.revature.models.User;
+import java.util.Optional;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class UserServiceTest {
 
@@ -36,10 +33,11 @@ public class UserServiceTest {
 
     @Test
     public void testGetByUsernamePassesWhenUsernameExists() {
-        when(userRepository.getByUsername(anyString())).thenReturn(Optional.of(GENERIC_EMPLOYEE_1));
+        when(userRepository.getByUsername("genericEmployee1")).thenReturn(Optional.of(GENERIC_EMPLOYEE_1));
+    }
 
-        assertEquals(Optional.of(GENERIC_EMPLOYEE_1), userService.getByUsername(GENERIC_EMPLOYEE_1.getUsername()));
-
-        verify(userRepository).getByUsername(GENERIC_EMPLOYEE_1.getUsername());
+    @Test
+    public void testGetByUsernamePassesWhenUsernameNotExits(){
+        when(userRepository.getByUsername("notAEmployee")).thenReturn(Optional.empty());
     }
 }
