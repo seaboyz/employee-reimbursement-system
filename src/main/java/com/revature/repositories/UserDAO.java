3b4989f4d78a0dcbb4973162568c28c1b8d935cf
seaboyz <seaboyz@hotmail.com>
2022-04-12 11:49:47 -0500

    Partial implemented UserDaoImplementation

--- src/main/java/com/revature/repositories/UserDAO.java
+++ src/main/java/com/revature/repositories/UserDAO.java
@@ -1,33 +1,19 @@
 package com.revature.repositories;
 
-import com.revature.database.MockDB;
+import java.sql.SQLException;
+import java.util.List;
+
 import com.revature.models.User;
 
-import java.util.ArrayList;
-import java.util.Optional;
+public interface UserDao {
+  public void add(User user) throws SQLException;
 
-public class UserDAO {
-  static ArrayList<User> users;
-  private MockDB mockDB = MockDB.getInstance();
+  public User getUserById(int id) throws SQLException;
 
+  public List<User> all() throws SQLException;
 
-  /**
-   * Should retrieve a User from the DB with the corresponding username or an empty optional if there is no match.
-   */
-  public Optional<User> getByUsername(String username) {
-    User user = mockDB.getUserByUsername(username);
-    return user == null ? Optional.empty() : Optional.of(user);
-  }
+  public void update(User user) throws SQLException;
 
-  /**
-   * <ul>
-   *     <li>Should Insert a new User record into the DB with the provided information.</li>
-   *     <li>Should throw an exception if the creation is unsuccessful.</li>
-   *     <li>Should return a User object with an updated ID.</li>
-   * </ul>
-   */
-  public User create(User userToBeRegistered) {
-    return userToBeRegistered;
-  }
+  public void delete(int id) throws SQLException;
 
 }
---
+++ src/main/java/com/revature/repositories/UserDAOImplementation.java
@@ -0,0 +1,43 @@
+package com.revature.repositories;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import com.revature.database.PostgreSQLDatabase;
+import com.revature.models.User;
+
+public class UserDAOImplementation implements UserDao {
+  static Connection conn = PostgreSQLDatabase.getConnection();
+
+  /**
+   * <ul>
+   * <li>Should Insert a new User record into the DB with the provided
+   * information.</li>
+   * <li>Should throw an exception if the creation is unsuccessful.</li>
+   * <li>Should return a User object with an updated ID.</li>
+   * </ul>
+   */
+  @Override
+  public void add(User newUser) throws SQLException {
+    String query = "INSERT INTO ERS_USERS "
+        + "(ERS_USER_NAME,ERS_PASSWORD,ERS_EMAIL,ERS_FIRST_NAME,ERS_LAST_NAME) "
+        + "VALUES "
+        + "(?, ?, ?, ? ?) ";
+    PreparedStatement ps = conn.prepareStatement(query);
+    ps.setString(1, newUser.getUsername());
+    ps.setString(2, newUser.getPassword());
+
+  }
+
+  /**
+   * Should retrieve a User from the DB with the corresponding username or an
+   * empty optional if there is no match.
+   */
+  public Optional<User> getByUsername(String username) {
+
+    return Optional.empty();
+  }
+
+}
