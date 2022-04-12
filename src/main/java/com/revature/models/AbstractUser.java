746706e8f3fd1a759ba760022c0554c2fb51f602
seaboyz <seaboyz@hotmail.com>
2022-04-12 11:55:44 -0500

    Add email getter and setter to AbstractUser

--- src/main/java/com/revature/models/AbstractUser.java
+++ src/main/java/com/revature/models/AbstractUser.java
@@ -8,13 +8,14 @@ import java.util.Objects;
  *
  * All users in this application must at least have:
  * <ul>
- *     <li>ID</li>
- *     <li>Username</li>
- *     <li>Password</li>
- *     <li>Role</li>
+ * <li>ID</li>
+ * <li>Username</li>
+ * <li>Password</li>
+ * <li>Role</li>
  * </ul>
  *
- * Additional fields can be added to the concrete {@link com.revature.models.User} class.
+ * Additional fields can be added to the concrete
+ * {@link com.revature.models.User} class.
  *
  * @author Center of Excellence
  */
@@ -24,18 +25,17 @@ public class AbstractUser {
     private int id;
     private String username;
     private String password;
+    private String email;
     private Role role;
 
-    public AbstractUser() {
-        super();
-    }
-
-    public AbstractUser(int id, String username, String password, Role role) {
+    public AbstractUser(int id, String username, String password, String email, Role role) {
         super();
         this.id = id;
         this.username = username;
         this.password = password;
+        this.email = email;
         this.role = role;
+
     }
 
     public int getId() {
@@ -62,6 +62,14 @@ public class AbstractUser {
         this.password = password;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public Role getRole() {
         return role;
     }
@@ -72,10 +80,13 @@ public class AbstractUser {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         AbstractUser that = (AbstractUser) o;
-        return id == that.id && Objects.equals(username, that.username) && Objects.equals(password, that.password) && role == that.role;
+        return id == that.id && Objects.equals(username, that.username) && Objects.equals(password, that.password)
+                && role == that.role;
     }
 
     @Override
