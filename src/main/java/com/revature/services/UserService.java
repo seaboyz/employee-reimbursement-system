c1b4e2289f11a91ec12b70e2be70fb01d5c8e215
seaboyz <seaboyz@hotmail.com>
2022-04-12 12:40:59 -0500

    Merge UserDaoImplementation

--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -1,37 +1,39 @@
 package com.revature.services;
 
-import com.revature.models.User;
-import com.revature.repositories.UserDAO;
-
 import java.util.Optional;
 
+import com.revature.models.User;
+import com.revature.repositories.UserDAOImplementation;
+
 /**
- * The UserService should handle the processing and retrieval of Users for the ERS application.
+ * The UserService should handle the processing and retrieval of Users for the
+ * ERS application.
  * <p>
  * {@code getByUsername} is the only method required;
  * however, additional methods can be added.
  * <p>
  * Examples:
  * <ul>
- *     <li>Create User</li>
- *     <li>Update User Information</li>
- *     <li>Get Users by ID</li>
- *     <li>Get Users by Email</li>
- *     <li>Get All Users</li>
+ * <li>Create User</li>
+ * <li>Update User Information</li>
+ * <li>Get Users by ID</li>
+ * <li>Get Users by Email</li>
+ * <li>Get All Users</li>
  * </ul>
  */
 public class UserService {
 
-  private final UserDAO userRepository;
+  private UserDAOImplementation userDaoImpl;
 
-  public UserService(UserDAO userRepository) {
-    this.userRepository = userRepository;
+  public UserService(UserDAOImplementation userDaoImpl) {
+    this.userDaoImpl = userDaoImpl;
   }
 
   /**
-   * Should retrieve a User with the corresponding username or an empty optional if there is no match.
+   * Should retrieve a User with the corresponding username or an empty optional
+   * if there is no match.
    */
   public Optional<User> getByUsername(String username) {
-    return userRepository.getByUsername(username);
+    return userDaoImpl.getByUsername(username);
   }
 }
