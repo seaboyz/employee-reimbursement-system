3018ed67f8a0d75f3684de7307c922865cfe59b2
seaboyz <seaboyz@hotmail.com>
2022-04-12 14:36:04 -0500

    Add UserDaoImplementationTest

---
+++ src/test/java/com/revature/repositories/UserDAOImplementationTest.java
@@ -0,0 +1,17 @@
+package com.revature.repositories;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class UserDAOImplementationTest {
+
+  @BeforeClass
+  void init() {
+
+  }
+
+  @Test
+  public void testGetByUsername() {
+
+  }
+}
