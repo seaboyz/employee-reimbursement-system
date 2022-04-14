74ef3ddb2a820207d59e8d053d5c8195efb5146a
seaboyz <seaboyz@hotmail.com>
2022-04-14 00:43:02 -0500

    Finish mocking database in the UserDAOImplementationTest

--- src/test/java/com/revature/repositories/UserDAOImplementationTest.java
+++ src/test/java/com/revature/repositories/UserDAOImplementationTest.java
@@ -2,52 +2,108 @@ package com.revature.repositories;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Optional;
 
 import com.revature.models.Role;
 import com.revature.models.User;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class UserDAOImplementationTest {
+  static final String EXIST_USERNAME = "EXIST_USERNAME";
+  static final String NONE_EXIST_USERNAME = "NONE_EXIST_USERNAME";
+
   UserDAOImplementation userDao;
+  User TEST_USER;
+
+  @Mock
+  User mockUser;
+
+  @Mock
+  Connection mockConnection;
+
+  @Mock
+  Statement mockStatement;
+
+  @Mock
+  ResultSet mockResultSet;
 
   @BeforeEach
-  public void init() {
-    userDao = new UserDAOImplementation();
+  public void init() throws SQLException {
+    userDao = new UserDAOImplementation(mockConnection);
+
+    when(mockConnection.createStatement()).thenReturn(mockStatement);
+
+    when(mockStatement.executeQuery(anyString())).thenReturn(mockResultSet);
+
+    TEST_USER = new User(1, "testUsername", "testPassword", "test@test.com", "testFirstname ", "testLastName",
+        Role.EMPLOYEE);
+
   }
 
-  @Test
-  public void testGetByUsernameShouldReturnOptional() throws Exception {
+  @Nested
+  @DisplayName("Test for return Optional")
+  class TestGetByUsernameReturnOptional {
+    @Test
+    public void testGetByUsernameShouldReturnOptionalWithNoneExistUsername() throws Exception {
+      when(mockResultSet.next()).thenReturn(false);
+      Optional<User> noneExitUser = userDao.getByUsername(NONE_EXIST_USERNAME);
+      assertEquals(noneExitUser.getClass(), Optional.class);
+    }
 
-    Optional<User> optionalUser = userDao.getByUsername("test");
-
-    assertEquals(optionalUser.getClass(), Optional.class);
-
-    ;
+    public void testGetByUsernameShouldReturnOptionalWithExistUsername() throws Exception {
+      when(mockResultSet.next()).thenReturn(true);
+      Optional<User> noneExitUser = userDao.getByUsername(EXIST_USERNAME);
+      assertEquals(noneExitUser.getClass(), Optional.class);
+      assertFalse(noneExitUser.isPresent());
+    }
   }
 
-  @Test
-  public void testGetByUsernameShouldReturnTheRightUser() throws Exception {
-    Optional<User> optionalUser = userDao.getByUsername("test");
-    User user = optionalUser.get();
+  @Nested
+  @DisplayName("Test for return right user")
+  class TestGetByUsername {
 
-    assertEquals(user.getUsername(), "test");
-    assertEquals(user.getEmail(), "test@test.com");
-    assertEquals(user.getPassword(), "123456");
-    assertEquals(user.getFirstname(), "john");
-    assertEquals(user.getLastname(), "doe");
-    assertEquals(user.getRole(), Role.EMPLOYEE);
+    @BeforeEach
+    void init() throws SQLException {
+      when(mockResultSet.next()).thenReturn(true);
+      when(mockResultSet.getInt("ers_user_id")).thenReturn(TEST_USER.getId());
+      when(mockResultSet.getString("ers_user_name")).thenReturn(TEST_USER.getUsername());
+      when(mockResultSet.getString("ers_password")).thenReturn(TEST_USER.getPassword());
+      when(mockResultSet.getString("ers_email")).thenReturn(TEST_USER.getEmail());
+      when(mockResultSet.getString("ers_first_name")).thenReturn(TEST_USER.getFirstname());
+      when(mockResultSet.getString("ers_last_name")).thenReturn(TEST_USER.getLastname());
+      when(mockResultSet.getInt("user_role_id")).thenReturn(1);
+    }
+
+    @Test
+    public void testGetByUsernameShouldReturnTheRightUser() throws Exception {
+
+      Optional<User> optionalUser = userDao.getByUsername(TEST_USER.getUsername());
+      User user = optionalUser.get();
+
+      assertEquals(optionalUser.getClass(), Optional.class);
+
+      assertEquals(user.getUsername(), TEST_USER.getUsername());
+      assertEquals(user.getEmail(), TEST_USER.getEmail());
+      assertEquals(user.getPassword(), TEST_USER.getPassword());
+      assertEquals(user.getFirstname(), TEST_USER.getFirstname());
+      assertEquals(user.getLastname(), TEST_USER.getLastname());
+      assertEquals(user.getRole(), TEST_USER.getRole());
+    }
   }
 
-  @Test
-  public void testGetByUsernameshouldResturnOptionalEmpty() throws Exception {
-
-    Optional<User> optionalNullUser = userDao.getByUsername("notExistUsername");
-    assertEquals(optionalNullUser.getClass(), Optional.class);
-
-    assertFalse(optionalNullUser.isPresent());
-  }
 }
