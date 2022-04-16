03b7b3321b580313fd86d44af4b61867e07df567
seaboyz <seaboyz@hotmail.com>
2022-04-16 15:16:54 -0500

    Add NOT_CURRENT_EMPLOYEE Role

--- src/main/java/com/revature/models/Role.java
+++ src/main/java/com/revature/models/Role.java
@@ -3,16 +3,17 @@ package com.revature.models;
 /**
  * Users within the ERS application are categorized within the following roles:
  * <ul>
- *     <li>Employee</li>
- *     <li>Finance Manager</li>
+ * <li>Employee</li>
+ * <li>Finance Manager</li>
  * </ul>
  *
  * Employees are the standard role for Users within the application.
  *
- * Finance Managers have additional permissions to process reimbursement requests.
+ * Finance Managers have additional permissions to process reimbursement
+ * requests.
  * <ul>
- *     <li>Finance Managers can submit reimbursement requests</li>
- *     <li>Finance Managers cannot process their own requests</li>
+ * <li>Finance Managers can submit reimbursement requests</li>
+ * <li>Finance Managers cannot process their own requests</li>
  * </ul>
  *
  * @author Center of Excellence
@@ -29,6 +30,12 @@ public enum Role {
         @Override
         public String toString() {
             return "Finance Manager";
+        }
+    },
+    NOT_CURRENT_EMPLOYEE {
+        @Override
+        public String toString() {
+            return "Not a current employee";
         }
     }
 
