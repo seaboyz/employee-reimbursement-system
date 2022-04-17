789b9beb88717217732563c9e76c854adc248d10
seaboyz <seaboyz@hotmail.com>
2022-04-17 05:03:53 -0500

    Rename loggedInUser to currentUser in View

--- src/main/java/com/revature/cli/Controller.java
+++ src/main/java/com/revature/cli/Controller.java
@@ -40,28 +40,6 @@ public class Controller {
 
   public User login(String username, String password)
       throws UserNamePasswordNotMatchException, UserNotExistException, SQLException {
-    // todo for REST API
-    // HttpClient POST http://localhost:8080/login
-    // https://hc.apache.org/httpcomponents-client-5.1.x/index.html
-
-    // CloseableHttpClient client = HttpClients.createDefault();
-    // HttpPost httpPost = new HttpPost("http://localhost:8080/api/user");
-    // List<NameValuePair> params = new ArrayList<>();
-    // params.add((new BasicNameValuePair("username", username)));
-    // params.add((new BasicNameValuePair("password", password)));
-    // httpPost.setEntity(new UrlEncodedFormEntity(params));
-    // CloseableHttpResponse response = client.execute(httpPost);
-
-    // if (response.getStatusLine().getStatusCode() == 200) {
-    // System.out.println("_______________________________");
-    // System.out.println("| Welcome |");
-    // System.out.println("| User Menu |");
-    // System.out.println("|_____________________________|");
-    // } else if (response.getStatusLine().getStatusCode() == 403) {
-    // System.out.println("The username and password was wrong, please try again.");
-    // System.out.println("Press 0 to go back to the main manu");
-    // }
-
     return authService.login(username, password);
   }
 
