5f5652c3dc171c0c709720f3ebbe78bd507e647b
seaboyz <seaboyz@hotmail.com>
2022-04-24 16:24:02 -0500

    Successfuly send user json PUT @/users/id

--- backend/src/main/java/com/revature/servlets/UserServlet.java
+++ backend/src/main/java/com/revature/servlets/UserServlet.java
@@ -1,6 +1,8 @@
 package com.revature.servlets;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -13,6 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.revature.database.PostgreSQLDatabase;
 import com.revature.models.User;
 import com.revature.repositories.UserDao;
@@ -50,14 +54,14 @@ public class UserServlet extends HttpServlet {
     res.setHeader("Access-Control-Allow-Methods", "GET");
   }
 
-  // for Prefligh
+  // * Preflight
   @Override
   protected void doOptions(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
     setAccessControlHeaders(res);
     res.setStatus(HttpServletResponse.SC_OK);
   }
 
-  // GET @users/{id}
+  // * GET @users/{id}
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
     setAccessControlHeaders(res);
@@ -73,7 +77,7 @@ public class UserServlet extends HttpServlet {
       res.setStatus(401);
       return;
     }
-    // ger pathInfo
+    // get userId
     String[] path = req.getPathInfo().split("/");
     int userId = Integer.parseInt(path[1]);
 
@@ -95,7 +99,6 @@ public class UserServlet extends HttpServlet {
           String jsonString = gson.toJson(user);
           out.println(jsonString);
         }
-
       } catch (SQLException e) {
         e.printStackTrace();
         res.setStatus(500);
@@ -104,6 +107,61 @@ public class UserServlet extends HttpServlet {
     }
 
     out.flush();
+
+  }
+
+  // * PUT @users/{id}
+  @Override
+  protected void doPut(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    setAccessControlHeaders(res);
+
+    // setup response
+    res.setContentType("text/html");
+    res.setCharacterEncoding("UTF-8");
+    PrintWriter out = res.getWriter();
+
+    // get token from req
+    String token = Util.getToken(req);
+    if (token == null) {
+      res.setStatus(401);
+      return;
+    }
+
+    // get userId
+    String[] path = req.getPathInfo().split("/");
+    int userId = Integer.parseInt(path[1]);
+
+    // verify token
+    if (!authService.isTokenValid(token)) {
+      res.setStatus(401);
+      out.println("<h2>Please login</h2>");
+      return;
+    }
+
+    // verify identity
+    if (!authService.isSelf(userId, token)) {
+      res.setStatus(401);
+      out.println("<h2>No permission allowed</h2>");
+      return;
+    }
+
+    // read json from request
+    InputStream requestStream = req.getInputStream();
+    InputStreamReader inputStreamReader = new InputStreamReader(requestStream);
+    JsonObject jsonObject = JsonParser.parseReader(inputStreamReader).getAsJsonObject();
+
+    // create new User object
+    String username = jsonObject.get("username").getAsString();
+    String email = jsonObject.get("email").getAsString();
+    String password = jsonObject.get("password").getAsString();
+
+    User userToBeUpdated = new User();
+    userToBeUpdated.setId(userId);
+    userToBeUpdated.setEmail(email);
+    userToBeUpdated.setPassword(password);
+    userToBeUpdated.setUsername(username);
+
+    out.println(gson.toJson(userToBeUpdated));
 
   }
 
