09908d1a16e89c62ee3821cb9561e47ab16d6f76
seaboyz <seaboyz@hotmail.com>
2022-04-24 11:22:51 -0500

    change userdaotest to integration test

--- backend/src/test/java/com/revature/repositories/UserDaoTest.java
+++ backend/src/test/java/com/revature/repositories/UserDaoTest.java
@@ -1,27 +1,20 @@
 package com.revature.repositories;
 
-import static org.junit.Assert.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Optional;
 
+import javax.servlet.UnavailableException;
+
+import com.revature.database.PostgreSQLDatabase;
 import com.revature.models.Role;
 import com.revature.models.User;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,51 +24,54 @@ public class UserDaoTest {
 
   UserDao userDao;
 
-  @Mock
-  User mockUser;
+  Connection connection;
 
-  @Mock
-  Connection mockConnection;
+  // @Mock
+  // User mockUser;
 
-  @Mock
-  Statement mockStatement;
+  // @Mock
+  // Connection mockConnection;
 
-  @Mock
-  ResultSet mockResultSet;
+  // @Mock
+  // Statement mockStatement;
 
-  @Mock
-  PreparedStatement mockPreparedStatement;
+  // @Mock
+  // ResultSet mockResultSet;
+
+  // @Mock
+  // PreparedStatement mockPreparedStatement;
 
   @BeforeEach
-  void init() {
-    userDao = new UserDao(mockConnection);
+  void init() throws SQLException, UnavailableException {
+    connection = PostgreSQLDatabase.getConnection();
+    userDao = new UserDao(connection);
   }
 
-  @Nested
-  class TestAddd {
-    @Test
-    void shouldReturnUserWithUpdatedId() throws SQLException {
-      User newUser = new User("test", "test@test.com", "123456", "john", "doe");
+  // @Nested
+  // class TestAdd {
+  // @Test
+  // void shouldReturnUserWithUpdatedId() throws SQLException {
+  // User newUser = new User("test", "test@test.com", "123456", "john", "doe");
 
-      String query = "INSERT INTO ERS_USERS "
-          + "(ERS_USER_NAME,ERS_PASSWORD,ERS_EMAIL,ERS_FIRST_NAME,ERS_LAST_NAME) "
-          + "VALUES "
-          + "(?, ?, ?, ?, ?)";
+  // String query = "INSERT INTO ERS_USERS "
+  // + "(ERS_USER_NAME,ERS_PASSWORD,ERS_EMAIL,ERS_FIRST_NAME,ERS_LAST_NAME) "
+  // + "VALUES "
+  // + "(?, ?, ?, ?, ?)";
 
-      when(mockConnection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS))
-          .thenReturn(mockPreparedStatement);
+  // when(mockConnection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS))
+  // .thenReturn(mockPreparedStatement);
 
-      when(mockPreparedStatement.execute()).thenReturn(true);
+  // when(mockPreparedStatement.execute()).thenReturn(true);
 
-      when(mockPreparedStatement.getGeneratedKeys()).thenReturn(mockResultSet);
+  // when(mockPreparedStatement.getGeneratedKeys()).thenReturn(mockResultSet);
 
-      when(mockResultSet.next()).thenReturn(true);
+  // when(mockResultSet.next()).thenReturn(true);
 
-      when(mockResultSet.getInt(1)).thenReturn(999);
+  // when(mockResultSet.getInt(1)).thenReturn(999);
 
-      assertEquals(999, userDao.add(newUser).getId());
-    }
-  }
+  // assertEquals(999, userDao.add(newUser).getId());
+  // }
+  // }
 
   @Nested
   class TestGetById {
@@ -83,97 +79,107 @@ public class UserDaoTest {
     @Test
     void getShouldReturnUserWithRoleWhenPassingId() throws SQLException {
 
-      User testUser = new User(1, "testUsername", "testPassword", "test@test.com", "testFirstname ", "testLastName",
-          Role.EMPLOYEE);
+      // User testUser = new User(1, "testUsername", "testPassword", "test@test.com",
+      // "testFirstname ", "testLastName",
+      // Role.EMPLOYEE);
 
-      when(mockConnection.createStatement()).thenReturn(mockStatement);
+      // when(mockConnection.createStatement()).thenReturn(mockStatement);
 
-      when(mockStatement.executeQuery(anyString())).thenReturn(mockResultSet);
+      // when(mockStatement.executeQuery(anyString())).thenReturn(mockResultSet);
 
-      when(mockResultSet.next()).thenReturn(true);
+      // when(mockResultSet.next()).thenReturn(true);
 
-      when(mockResultSet.getInt("ers_user_id")).thenReturn(testUser.getId());
-      when(mockResultSet.getString("ers_user_name")).thenReturn(testUser.getUsername());
-      when(mockResultSet.getString("ers_password")).thenReturn(testUser.getPassword());
-      when(mockResultSet.getString("ers_email")).thenReturn(testUser.getEmail());
-      when(mockResultSet.getString("ers_first_name")).thenReturn(testUser.getFirstname());
-      when(mockResultSet.getInt("user_role_id")).thenReturn(1);
+      // when(mockResultSet.getInt("ers_user_id")).thenReturn(testUser.getId());
+      // when(mockResultSet.getString("ers_user_name")).thenReturn(testUser.getUsername());
+      // when(mockResultSet.getString("ers_password")).thenReturn(testUser.getPassword());
+      // when(mockResultSet.getString("ers_email")).thenReturn(testUser.getEmail());
+      // when(mockResultSet.getString("ers_first_name")).thenReturn(testUser.getFirstname());
+      // when(mockResultSet.getInt("user_role_id")).thenReturn(1);
 
-      assertEquals(Optional.class, userDao.get(1).getClass());
-      assertEquals(testUser, userDao.get(1).get());
+      // assertEquals(Optional.class, userDao.get(1).getClass());
+      // assertEquals(testUser, userDao.get(1).get());
+      User user = userDao.get(34).get();
+
+      assertEquals(User.class, user.getClass());
+      assertEquals(34, user.getId());
+      assertEquals(Role.EMPLOYEE, user.getRole());
     }
   }
 
-  @Nested
-  class TestGetByUsername {
-    User TEST_USER;
+  // @Nested
+  // class TestGetByUsername {
+  // User TEST_USER;
 
-    @BeforeEach
-    public void init() throws SQLException {
+  // @BeforeEach
+  // public void init() throws SQLException {
 
-      when(mockConnection.createStatement()).thenReturn(mockStatement);
+  // when(mockConnection.createStatement()).thenReturn(mockStatement);
 
-      when(mockStatement.executeQuery(anyString())).thenReturn(mockResultSet);
+  // when(mockStatement.executeQuery(anyString())).thenReturn(mockResultSet);
 
-      TEST_USER = new User(1, "testUsername", "testPassword", "test@test.com", "testFirstname ", "testLastName",
-          Role.EMPLOYEE);
-    }
+  // TEST_USER = new User(1, "testUsername", "testPassword", "test@test.com",
+  // "testFirstname ", "testLastName",
+  // Role.EMPLOYEE);
+  // }
 
-    @Test
-    public void testGetByUsernameShouldReturnOptionalWithNoneExistUsername() throws Exception {
-      when(mockResultSet.next()).thenReturn(false);
-      Optional<User> noneExitUser = userDao.get(NONE_EXIST_USERNAME);
-      assertEquals(noneExitUser.getClass(), Optional.class);
-    }
+  // @Test
+  // public void testGetByUsernameShouldReturnOptionalWithNoneExistUsername()
+  // throws Exception {
+  // when(mockResultSet.next()).thenReturn(false);
+  // Optional<User> noneExitUser = userDao.get(NONE_EXIST_USERNAME);
+  // assertEquals(noneExitUser.getClass(), Optional.class);
+  // }
 
-    public void testGetByUsernameShouldReturnOptionalWithExistUsername() throws Exception {
-      when(mockResultSet.next()).thenReturn(true);
-      Optional<User> noneExitUser = userDao.get(EXIST_USERNAME);
-      assertEquals(noneExitUser.getClass(), Optional.class);
-      assertFalse(noneExitUser.isPresent());
-    }
+  // public void testGetByUsernameShouldReturnOptionalWithExistUsername() throws
+  // Exception {
+  // when(mockResultSet.next()).thenReturn(true);
+  // Optional<User> noneExitUser = userDao.get(EXIST_USERNAME);
+  // assertEquals(noneExitUser.getClass(), Optional.class);
+  // assertFalse(noneExitUser.isPresent());
+  // }
 
-    @Disabled
-    @Test
-    public void testGetByUsernameShouldReturnTheRightUser() throws Exception {
-      when(mockResultSet.next()).thenReturn(true);
-      when(mockResultSet.getInt("ers_user_id")).thenReturn(TEST_USER.getId());
-      when(mockResultSet.getString("ers_user_name")).thenReturn(TEST_USER.getUsername());
-      when(mockResultSet.getString("ers_password")).thenReturn(TEST_USER.getPassword());
-      when(mockResultSet.getString("ers_email")).thenReturn(TEST_USER.getEmail());
-      when(mockResultSet.getString("ers_first_name")).thenReturn(TEST_USER.getFirstname());
-      when(mockResultSet.getString("ers_last_name")).thenReturn(TEST_USER.getLastname());
-      when(mockResultSet.getInt("user_role_id")).thenReturn(1);
+  // @Disabled
+  // @Test
+  // public void testGetByUsernameShouldReturnTheRightUser() throws Exception {
+  // when(mockResultSet.next()).thenReturn(true);
+  // when(mockResultSet.getInt("ers_user_id")).thenReturn(TEST_USER.getId());
+  // when(mockResultSet.getString("ers_user_name")).thenReturn(TEST_USER.getUsername());
+  // when(mockResultSet.getString("ers_password")).thenReturn(TEST_USER.getPassword());
+  // when(mockResultSet.getString("ers_email")).thenReturn(TEST_USER.getEmail());
+  // when(mockResultSet.getString("ers_first_name")).thenReturn(TEST_USER.getFirstname());
+  // when(mockResultSet.getString("ers_last_name")).thenReturn(TEST_USER.getLastname());
+  // when(mockResultSet.getInt("user_role_id")).thenReturn(1);
 
-      Optional<User> optionalUser = userDao.get(TEST_USER.getUsername());
-      User user = optionalUser.get();
+  // Optional<User> optionalUser = userDao.get(TEST_USER.getUsername());
+  // User user = optionalUser.get();
 
-      assertEquals(optionalUser.getClass(), Optional.class);
+  // assertEquals(optionalUser.getClass(), Optional.class);
 
-      assertEquals(user.getUsername(), TEST_USER.getUsername());
-      assertEquals(user.getEmail(), TEST_USER.getEmail());
-      assertEquals(user.getPassword(), TEST_USER.getPassword());
-      assertEquals(user.getFirstname(), TEST_USER.getFirstname());
-      assertEquals(user.getLastname(), TEST_USER.getLastname());
-      assertEquals(user.getRole(), TEST_USER.getRole());
-    }
-  }
+  // assertEquals(user.getUsername(), TEST_USER.getUsername());
+  // assertEquals(user.getEmail(), TEST_USER.getEmail());
+  // assertEquals(user.getPassword(), TEST_USER.getPassword());
+  // assertEquals(user.getFirstname(), TEST_USER.getFirstname());
+  // assertEquals(user.getLastname(), TEST_USER.getLastname());
+  // assertEquals(user.getRole(), TEST_USER.getRole());
+  // }
+  // }
 
-  @Nested
-  class TestUpdate {
+  // @Nested
+  // class TestUpdate {
 
-    @Test
-    void ShouldReturnCopyOfTheUserToBeUpdated() throws SQLException {
-      when(mockConnection.prepareStatement(anyString())).thenReturn(mockPreparedStatement);
+  // @Test
+  // void ShouldReturnCopyOfTheUserToBeUpdated() throws SQLException {
+  // when(mockConnection.prepareStatement(anyString())).thenReturn(mockPreparedStatement);
 
-      when(mockPreparedStatement.executeUpdate()).thenReturn(1);
+  // when(mockPreparedStatement.executeUpdate()).thenReturn(1);
 
-      User testUser = new User(1, "testUsername", "testPassword", "test@test.com", "testFirstname ", "testLastName",
-          Role.EMPLOYEE);
+  // User testUser = new User(1, "testUsername", "testPassword", "test@test.com",
+  // "testFirstname ", "testLastName",
+  // Role.EMPLOYEE);
 
-      assertEquals(userDao.update(testUser), testUser);
+  // assertEquals(userDao.update(testUser), testUser);
 
-      assertNotSame(userDao.update(testUser), testUser);
-    }
-  }
+  // assertNotSame(userDao.update(testUser), testUser);
+  // }
+  // }
 }
